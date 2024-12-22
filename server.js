162b6d787887561280726cf77efa0ca75228bd3c
kate_sarant <k.sarantopoulou@itlab.gr>
2024-12-22 15:31:34 +0200

    bootstrap update

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.get('/login', (req, res) => {
   res.render('login');
 });
 
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
 app.get('/posts', (req, res) => {
   res.render('posts');
 });
